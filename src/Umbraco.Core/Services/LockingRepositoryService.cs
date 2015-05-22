4d8768f1d9bbaa8a127f84b0f349a1c09dd63112
Stephan <stephane@umbraco.dk>
2015-05-22 10:02:08 +0200

    Refactor/cleanup LockingRepository

--- src/Umbraco.Core/Services/LockingRepositoryService.cs
+++ src/Umbraco.Core/Services/LockingRepositoryService.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.Repositories;
 using Umbraco.Core.Persistence.UnitOfWork;
 
 namespace Umbraco.Core.Services
 {
-    // FIXME the issue here is that if the service is public, the repository needs to be public too?!
-    // so either we fix what's public and what's not (eg ContentService) or we cannot do this with a base class
-    // and have to do it with some sort of helper... sort-of
-    // private static readonly LockingRepository<ContentRepository> _lockingRepo = new LockingRepository<ContentRepository>(...)
-
     internal class LockingRepository<TRepository>
-        where TRepository : IDisposable
+        where TRepository : IDisposable, IRepository
     {
         private readonly IDatabaseUnitOfWorkProvider _uowProvider;
         private readonly Func<IDatabaseUnitOfWork, TRepository> _repositoryFactory;
@@ -33,7 +28,7 @@ namespace Umbraco.Core.Services
             _writeLockIds = writeLockIds == null ? new int[0] : writeLockIds.ToArray();
         }
 
-        public void WithReadLocked(Action<LockedRepository> action, bool autoCommit = true)
+        public void WithReadLocked(Action<LockedRepository<TRepository>> action, bool autoCommit = true)
         {
             var uow = _uowProvider.GetUnitOfWork();
             using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
@@ -43,7 +38,7 @@ namespace Umbraco.Core.Services
 
                 using (var repository = _repositoryFactory(uow))
                 {
-                    action(new LockedRepository(transaction, uow, repository));
+                    action(new LockedRepository<TRepository>(transaction, uow, repository));
                     if (autoCommit == false) return;
                     uow.Commit();
                     transaction.Complete();
@@ -51,7 +46,7 @@ namespace Umbraco.Core.Services
             }
         }
 
-        public TResult WithReadLocked<TResult>(Func<LockedRepository, TResult> func, bool autoCommit = true)
+        public TResult WithReadLocked<TResult>(Func<LockedRepository<TRepository>, TResult> func, bool autoCommit = true)
         {
             var uow = _uowProvider.GetUnitOfWork();
             using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
@@ -61,7 +56,7 @@ namespace Umbraco.Core.Services
 
                 using (var repository = _repositoryFactory(uow))
                 {
-                    var ret = func(new LockedRepository(transaction, uow, repository));
+                    var ret = func(new LockedRepository<TRepository>(transaction, uow, repository));
                     if (autoCommit == false) return ret;
                     uow.Commit();
                     transaction.Complete();
@@ -70,7 +65,7 @@ namespace Umbraco.Core.Services
             }
         }
 
-        public void WithWriteLocked(Action<LockedRepository> action, bool autoCommit = true)
+        public void WithWriteLocked(Action<LockedRepository<TRepository>> action, bool autoCommit = true)
         {
             var uow = _uowProvider.GetUnitOfWork();
             using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
@@ -80,7 +75,7 @@ namespace Umbraco.Core.Services
 
                 using (var repository = _repositoryFactory(uow))
                 {
-                    action(new LockedRepository(transaction, uow, repository));
+                    action(new LockedRepository<TRepository>(transaction, uow, repository));
                     if (autoCommit == false) return;
                     uow.Commit();
                     transaction.Complete();
@@ -88,7 +83,7 @@ namespace Umbraco.Core.Services
             }
         }
 
-        public TResult WithWriteLocked<TResult>(Func<LockedRepository, TResult> func, bool autoCommit = true)
+        public TResult WithWriteLocked<TResult>(Func<LockedRepository<TRepository>, TResult> func, bool autoCommit = true)
         {
             var uow = _uowProvider.GetUnitOfWork();
             using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
@@ -98,7 +93,7 @@ namespace Umbraco.Core.Services
 
                 using (var repository = _repositoryFactory(uow))
                 {
-                    var ret = func(new LockedRepository(transaction, uow, repository));
+                    var ret = func(new LockedRepository<TRepository>(transaction, uow, repository));
                     if (autoCommit == false) return ret;
                     uow.Commit();
                     transaction.Complete();
@@ -106,129 +101,80 @@ namespace Umbraco.Core.Services
                 }
             }
         }
+    }
 
-        public class LockedRepository
+    internal class LockedRepository<TRepository>
+        where TRepository : IDisposable, IRepository
+    {
+        public LockedRepository(Transaction transaction, IDatabaseUnitOfWork unitOfWork, TRepository repository)
         {
-            public LockedRepository(Transaction transaction, IDatabaseUnitOfWork unitOfWork, TRepository repository)
-            {
-                Transaction = transaction;
-                UnitOfWork = unitOfWork;
-                Repository = repository;
-            }
+            Transaction = transaction;
+            UnitOfWork = unitOfWork;
+            Repository = repository;
+        }
 
-            public Transaction Transaction { get; private set; }
-            public IDatabaseUnitOfWork UnitOfWork { get; private set; }
-            public TRepository Repository { get; private set; }
+        public Transaction Transaction { get; private set; }
+        public IDatabaseUnitOfWork UnitOfWork { get; private set; }
+        public TRepository Repository { get; private set; }
 
-            public void Commit()
-            {
-                UnitOfWork.Commit();
-                Transaction.Complete();
-            }
+        public void Commit()
+        {
+            UnitOfWork.Commit();
+            Transaction.Complete();
         }
     }
 
+    // this is how we'd implement LockingRepositoryService and it initially sounded like a great
+    // idea, but afterwards I think what we have at the moment ie explicitely creating the lrepo
+    // in the service, is easier to understand...
+    //
+    // also requires that LockedRepository<TRepository> is made public
+
+    /*
     public abstract class LockingRepositoryService<TRepository> : RepositoryService
-        where TRepository : IDisposable
+        where TRepository : IDisposable, IRepository
     {
-        protected LockingRepositoryService(IDatabaseUnitOfWorkProvider provider, RepositoryFactory repositoryFactory, ILogger logger)
+        private readonly LockingRepository<TRepository> _lrepo;
+
+        protected LockingRepositoryService(IDatabaseUnitOfWorkProvider provider, RepositoryFactory repositoryFactory,
+            ILogger logger)
             : base(provider, repositoryFactory, logger)
-        { }
+        {
+            // properties would need to return constants, because the
+            // inheriting classes constructors have not run yet, only
+            // their initializers
+
+            // ReSharper disable DoNotCallOverridableMethodsInConstructor
+            var readLockIds = LockingRepositoryReadLockIds;
+            var writeLockIds = LockingRepositoryWriteLockIds;
+            // ReSharper restore DoNotCallOverridableMethodsInConstructor
+
+            _lrepo = new LockingRepository<TRepository>(provider, CreateRepository, readLockIds, writeLockIds);
+        }
 
         protected abstract TRepository CreateRepository(IDatabaseUnitOfWork unitOfWork);
+        protected abstract IEnumerable<int> LockingRepositoryReadLockIds { get; }
+        protected abstract IEnumerable<int> LockingRepositoryWriteLockIds { get; } 
 
-        protected void WithReadLocked(int[] lockIds, Action<LockedRepository> action, bool autoCommit = true)
+        protected void WithReadLocked(int[] lockIds, Action<LockedRepository<TRepository>> action, bool autoCommit = true)
         {
-            var uow = UowProvider.GetUnitOfWork();
-            using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
-            {
-                foreach (var lockId in lockIds)
-                    uow.Database.AcquireLockNodeReadLock(lockId);
-
-                using (var repository = CreateRepository(uow))
-                {
-                    action(new LockedRepository(transaction, uow, repository));
-                    if (autoCommit == false) return;
-                    uow.Commit();
-                    transaction.Complete();
-                }
-            }
+            _lrepo.WithReadLocked(action, autoCommit);
         }
 
-        protected TResult WithReadLocked<TResult>(int[] lockIds, Func<LockedRepository, TResult> func, bool autoCommit = true)
+        protected TResult WithReadLocked<TResult>(int[] lockIds, Func<LockedRepository<TRepository>, TResult> func, bool autoCommit = true)
         {
-            var uow = UowProvider.GetUnitOfWork();
-            using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
-            {
-                foreach (var lockId in lockIds)
-                    uow.Database.AcquireLockNodeReadLock(lockId);
-
-                using (var repository = CreateRepository(uow))
-                {
-                    var ret = func(new LockedRepository(transaction, uow, repository));
-                    if (autoCommit == false) return ret;
-                    uow.Commit();
-                    transaction.Complete();
-                    return ret;
-                }
-            }
+            return _lrepo.WithReadLocked(func, autoCommit);
         }
 
-        protected void WithWriteLocked(int[] lockIds, Action<LockedRepository> action, bool autoCommit = true)
+        protected void WithWriteLocked(int[] lockIds, Action<LockedRepository<TRepository>> action, bool autoCommit = true)
         {
-            var uow = UowProvider.GetUnitOfWork();
-            using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
-            {
-                foreach (var lockId in lockIds)
-                    uow.Database.AcquireLockNodeReadLock(lockId);
-
-                using (var repository = CreateRepository(uow))
-                {
-                    action(new LockedRepository(transaction, uow, repository));
-                    if (autoCommit == false) return;
-                    uow.Commit();
-                    transaction.Complete();
-                }
-            }
+            _lrepo.WithWriteLocked(action, autoCommit);
         }
 
-        protected TResult WithWriteLocked<TResult>(int[] lockIds, Func<LockedRepository, TResult> func, bool autoCommit = true)
+        protected TResult WithWriteLocked<TResult>(int[] lockIds, Func<LockedRepository<TRepository>, TResult> func, bool autoCommit = true)
         {
-            var uow = UowProvider.GetUnitOfWork();
-            using (var transaction = uow.Database.GetTransaction(IsolationLevel.RepeatableRead))
-            {
-                foreach (var lockId in lockIds)
-                    uow.Database.AcquireLockNodeReadLock(lockId);
-
-                using (var repository = CreateRepository(uow))
-                {
-                    var ret = func(new LockedRepository(transaction, uow, repository));
-                    if (autoCommit == false) return ret;
-                    uow.Commit();
-                    transaction.Complete();
-                    return ret;
-                }
-            }
-        }
-
-        protected class LockedRepository
-        {
-            public LockedRepository(Transaction transaction, IDatabaseUnitOfWork unitOfWork, TRepository repository)
-            {
-                Transaction = transaction;
-                UnitOfWork = unitOfWork;
-                Repository = repository;
-            }
-
-            public Transaction Transaction { get; private set; }
-            public IDatabaseUnitOfWork UnitOfWork { get; private set; }
-            public TRepository Repository { get; private set; }
-
-            public void Commit()
-            {
-                UnitOfWork.Commit();
-                Transaction.Complete();
-            }
+            return _lrepo.WithWriteLocked(func, autoCommit);
         }
     }
+    */
 }
