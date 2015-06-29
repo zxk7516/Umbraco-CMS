a2f679051b332ad2b51fcc0697b71e2f5e889100
Stephan <stephane@umbraco.dk>
2015-06-29 16:03:36 +0200

    Fix migrations

---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/AddCacheInstructionOriginatedColumn.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades
+{
+    [Migration("7.3.0", 2, GlobalSettings.UmbracoMigrationName)]
+    public class AddCacheInstructionOriginatedColumn : MigrationBase
+    {
+        public AddCacheInstructionOriginatedColumn(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        { }
+
+        public override void Up()
+        {
+            // don't execute if the column is already there
+            var columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToArray();
+            if (columns.Any(x => x.TableName.InvariantEquals("umbracoCacheInstruction") && x.ColumnName.InvariantEquals("originated")) == false)
+                Create.Column("originated").OnTable("umbracoCacheInstruction").AsString(500).NotNullable();
+        }
+
+        public override void Down()
+        {
+            // nothing - going down, the table will be removed anyway
+        }
+    }
+}
