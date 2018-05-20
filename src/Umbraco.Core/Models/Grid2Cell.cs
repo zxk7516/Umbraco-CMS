ab82d3b05365033a592704ae60d67196e96b3bc2
mattbrailsford <me@mattbrailsford.com>
2018-05-20 16:23:15 +0100

    Added Grid2Value model

---
+++ src/Umbraco.Core/Models/Grid2Cell.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Core.Models
+{
+    public class Grid2Cell
+    {
+        public int Colspan { get; set; }
+
+        public IPublishedElement Settings { get; set; }
+            
+        public IEnumerable<IPublishedElement> Items { get; set; }
+    }
+}
