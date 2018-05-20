40e986d5dc5b462bfecabe2a567f5a4eebfa484c
Shannon <sdeminick@gmail.com>
2018-05-20 15:56:40 +0200

    Adds grid.resource

---
+++ src/Umbraco.Web.UI.Client/src/common/resources/grid.resource.js
@@ -0,0 +1,20 @@
+﻿/**
+  * @ngdoc service
+  * @name umbraco.resources.gridResource
+  * @description Handles retrieving grid data
+  **/
+function gridResource($http, umbRequestHelper) {
+    return {
+
+        getCultures: function () {
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "gridApiBaseUrl",
+                        "GetContentTypes")),
+                "Failed to get grid content types");
+        }
+    };
+}
+
+angular.module('umbraco.resources').factory('gridResource', gridResource);
