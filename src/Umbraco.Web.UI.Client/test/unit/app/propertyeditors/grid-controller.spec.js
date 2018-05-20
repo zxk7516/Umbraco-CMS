8502e10900de9da5c8dd86ce9d216a66f5c253cd
Lars-Erik Aabech <lea@markedspartner.no>
2018-05-20 16:10:26 +0200

    changed schema for grid data in spec to what we're aiming at

--- src/Umbraco.Web.UI.Client/test/unit/app/propertyeditors/grid-controller.spec.js
+++ src/Umbraco.Web.UI.Client/test/unit/app/propertyeditors/grid-controller.spec.js
@@ -1,4 +1,4 @@
-describe("grid 2", function() {
+describe("grid 2", function () {
 
     var controller,
         scope,
@@ -13,10 +13,10 @@ describe("grid 2", function() {
                 "render": null,
                 "icon": "icon-coin",
                 "config": {
-                  "style": "font-size: 36px; line-height: 45px; font-weight: bold",
-                  "markup": "<h1>#value#</h1>"
+                    "style": "font-size: 36px; line-height: 45px; font-weight: bold",
+                    "markup": "<h1>#value#</h1>"
                 }
-              }
+            }
         ];
 
     function outputModel() {
@@ -25,50 +25,97 @@ describe("grid 2", function() {
 
     beforeEach(module('umbraco'));
 
-    beforeEach(inject(function(
+    beforeEach(inject(function (
         $rootScope,
         $controller,
         $q
-    ){
+    ) {
         q = $q;
         scope = $rootScope.$new();
-        
+
         scope.model = {
             config: {
-                items: {
-                    config: {
-
-                    },
-                    templates: [
-                        {
-                            "name":"1 column layout",
-                            "sections":[  
-                                {  
-                                    "grid":12
-                                }
-                            ]
-                        }
-                    ],
-                    layouts: [  
-                        {  
-                            "name":"Full width",
-                            "areas":[  
-                                {  
-                                    "grid":12,
-                                    "allowAll":false,
-                                    "allowed":[  
-                                        "headline"
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
+                columns: 12,
+                rows: [
+                    {
+                        alias: "fullwidth",
+                        Name: "Full width",
+                        settingsType: "5C25DA30-822E-4E39-BDD5-1D86058323E3",
+                        cells: [
+                            {
+                                colspan: 8,
+                                // Main column settings
+                                settingsType: "B994CB2F-D5A0-48DD-A8BA-AD8E4970B216",
+                                allowAll: false,
+                                allowed: [
+                                    "84ADAEB2-BB42-4069-BCA8-52605158ECD2"
+                                ]
+                            },
+                            {
+                                colspan: 4,
+                                // Sidebar settings
+                                settingsType: "5DBC34A6-FEF5-4169-93BB-05CAB5344663",
+                                allowAll: true,
+                                allowed: []
+                            }
+                        ]
+                    }
+                ]
+            },
+            value: {
+                rows: [
+                    {
+                        alias: "fullwidth",
+                        settings: {
+                            classNames: "fancy row",
+                            backgroundImage: "0BAD7ABF-F423-4336-A6A4-F00AF1815971"
+                        },
+                        cells: [
+                            {
+                                settings: {
+                                    type: "B994CB2F-D5A0-48DD-A8BA-AD8E4970B216",
+                                    values: {
+                                        classNames: "fancy cell",
+                                        backgroundImage: "9805F5A9-D6F7-4981-88F7-2F2644ED0759"
+                                    }
+                                },
+                                items: [
+                                    {
+                                        type: "84ADAEB2-BB42-4069-BCA8-52605158ECD2",
+                                        values: {
+                                            "headline": "Welcome to the fantastic site"
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                settings: {
+                                    type: "5DBC34A6-FEF5-4169-93BB-05CAB5344663",
+                                    values: {
+                                        classNames: "sidebar cell",
+                                        sidebarSetting: "some other setting"
+                                    }
+                                },
+                                items: [
+                                    {
+                                        type: "DBAD0C5C-95F2-4DF8-A888-C84B24DA1962",
+                                        values: {
+                                            "links": [
+                                                "47873858-D92B-4D9F-90C0-04A6C14954FF",
+                                                "F0814EF0-E356-44F3-911F-AF2A1B209223"
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
         };
 
         gridService = {
-            getGridEditors: function() {
+            getGridEditors: function () {
                 var def = q.defer();
                 def.resolve([]);
                 return def.promise;
@@ -76,12 +123,12 @@ describe("grid 2", function() {
         }
 
         angularHelper = {
-            getCurrentForm: function() {
+            getCurrentForm: function () {
                 return {
-                    $setDirty: function() {
+                    $setDirty: function () {
                         var iRememberBeingDirty = true;
                     }
-                } 
+                }
             }
         }
 
@@ -94,18 +141,18 @@ describe("grid 2", function() {
         scope.$digest();
     }));
 
-    it("defaults to 12 columns", function() {
+    it("defaults to 12 columns", function () {
         expect(scope.model.config.items.columns).toBe(12);
     });
 
-    it("when only one layout and row config, adds layout and row", function() {
+    it("when only one layout and row config, adds layout and row", function () {
         expect(scope.model.value.sections[0].rows[0]).toBeDefined();
     });
 
-    it("shows add editor dialog", function() {
+    it("shows add editor dialog", function () {
         expect(scope.editorOverlay).toBeUndefined();
         scope.openEditorOverlay(
-            {}, 
+            {},
             scope.model.value.sections[0].rows[0].areas[0],
             0,
             ""
@@ -115,10 +162,10 @@ describe("grid 2", function() {
         }));
     });
 
-    it("adds editor to cell", function() {
+    it("adds editor to cell", function () {
         scope.addControl(
-            gridEditors[0], 
-            scope.model.value.sections[0].rows[0].areas[0], 
+            gridEditors[0],
+            scope.model.value.sections[0].rows[0].areas[0],
             0
         );
 
