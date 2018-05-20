c7aae533ad5e812831ce66a72d92e240036cea22
Shannon <sdeminick@gmail.com>
2018-05-20 14:47:20 +0200

    Creates c# controller for getting grid cell scaffold and meta data

---
+++ src/Umbraco.Web/Editors/GridController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.IO;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web.Models.ContentEditing;
+
+namespace Umbraco.Web.Editors
+{
+    public class GridController : UmbracoAuthorizedJsonController
+    {
+        public IEnumerable<GridContentType> GetContentTypes()
+        {
+            //fixme - the content types returned will need to come from the grid data type config
+            var contentTypes = Services.ContentTypeService.GetAll();
+
+            return contentTypes.Select(x => new GridContentType
+            {
+                Alias = x.Alias,
+                Icon = x.Icon,
+                Id = x.Id,
+                Key = x.Key,
+                Name = x.Name,
+                View = GetViewPath(x)
+            });
+        }
+
+        public GridContentCell GetScaffold(Guid guid)
+        {
+            var contentType = Services.ContentTypeService.Get(guid);
+            if (contentType == null) throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            var emptyContent = Services.ContentService.Create("", -1, contentType.Alias, Security.GetUserId().ResultOr(0));
+            var mapped = Mapper.Map<GridContentCell>(emptyContent);
+
+            //remove this tab if it exists: umbContainerView
+            var containerTab = mapped.Tabs.FirstOrDefault(x => x.Alias == Core.Constants.Conventions.PropertyGroups.ListViewGroupName);
+            mapped.Tabs = mapped.Tabs.Except(new[] { containerTab });            
+
+            return mapped;
+        }
+
+        private string GetViewPath(IContentType contentType)
+        {
+            var alias = contentType.Alias;
+            var path = $"views/propertyeditors/grid2/inlineeditors/{alias}.html";
+            var file = IOHelper.MapPath($"~{GlobalSettings.Path}/{path}");
+            return System.IO.File.Exists(file) ? path : null; 
+        }
+    }
+}
---
+++ src/Umbraco.Web/Models/ContentEditing/GridContentCell.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Web.Models.ContentEditing
+{
+    [DataContract(Name = "gridCell", Namespace = "")]
+    public class GridContentCell : TabbedContentItem<ContentPropertyDisplay, IContentBase>
+    {
+
+    }
+}
